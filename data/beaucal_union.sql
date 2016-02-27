9015da32e494e2974f7691c4785dabe2618ea816
Derek Illchuk <dillchuk@gmail.com>
2016-02-27 11:57:06 -0600

    `set` is NULLable, so records can be manually deleted without foreign key problems.  i.e. first set parent to NULL.

--- data/beaucal_union.sql
+++ data/beaucal_union.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `beaucal_union` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `item` varchar(255) NOT NULL UNIQUE KEY,
-  `set` varchar(255) NOT NULL
+  `set` varchar(255) NULL #to manually delete record, first set this to NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ALTER TABLE `beaucal_union` ADD INDEX(`set`);
 ALTER TABLE `beaucal_union` ADD FOREIGN KEY (`set`)
